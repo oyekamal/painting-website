235daf1b3a2c2d30b6c8fe3e7a116ba424796fa7
oyekamal <autocratickamal1@gmail.com>
2023-03-08 12:26:55 +0500

    frontend added

---
+++ app/store/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import get_object_or_404, redirect, render
+
+from django.db.models import Q
+
+
+# Create your views here.
+
+
+def home(request):
+    return render(request, "store/home.html")
